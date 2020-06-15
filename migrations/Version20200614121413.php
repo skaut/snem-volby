63e91853ea17619c752e98dc3ed71316b997940e
František Hána <sinacek@gmail.com>
2020-06-15 09:04:05 +0200

    Update Version20200614121413.php

--- migrations/Version20200614121413.php
+++ migrations/Version20200614121413.php
@@ -7,26 +7,16 @@ namespace Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20200614121413 extends AbstractMigration
 {
-    public function getDescription() : string
-    {
-        return '';
-    }
-
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE users_vote (id INT AUTO_INCREMENT NOT NULL, person_id INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, `option` VARCHAR(255) NOT NULL COMMENT \'(DC2Type:string_enum)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE users_vote');
         $this->addSql('DROP TABLE vote');
     }
