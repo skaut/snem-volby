aa7244a58b6ac5d25b4560746b5430677d032a51
Honza Kosák <kosak@webovy-servis.cz>
2020-06-15 20:19:41 +0200

    cas hlasovani

--- app/Components/VotingStateBox.php
+++ app/Components/VotingStateBox.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Components;
 
 use App\AuthenticatedModule\Components\BaseControl;
+use DateTimeImmutable;
 use eGen\MessageBus\Bus\QueryBus;
+use Model\Vote;
 
 final class VotingStateBox extends BaseControl
 {
@@ -20,6 +22,13 @@ final class VotingStateBox extends BaseControl
     public function render() : void
     {
         $this->template->setFile(__DIR__ . '/templates/VotingStateBox.latte');
+
+        $this->template->setParameters([
+            'now' => new DateTimeImmutable(),
+            'beginAt' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', Vote::VOTING_BEGIN_AT),
+            'endAt' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', Vote::VOTING_END_AT),
+        ]);
+
         $this->template->render();
     }
 }
