1b6090ef1eea48bc21263811d161f0d56326718b
Honza Kosák <kosak@webovy-servis.cz>
2020-06-16 14:22:00 +0200

    sjednocení VotingBeginQuery a VotingEndQuery do VotingTimeQuery

---
+++ app/Model/Vote/ReadModel/Queries/VotingTimeQuery.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Vote\ReadModel\Queries;
+
+use Model\Vote\ReadModel\QueryHandlers\VotingTimeQueryHandler;
+
+/**
+ * @see VotingTimeQueryHandler
+ */
+class VotingTimeQuery
+{
+}
---
+++ app/Model/Vote/ReadModel/QueryHandlers/VotingTimeQueryHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Vote\ReadModel\QueryHandlers;
+
+use Model\Vote\ReadModel\Queries\VotingTimeQuery;
+use Model\Vote\VotingTime;
+
+final class VotingTimeQueryHandler
+{
+    private const VOTING_BEGIN_AT = '2020-06-25 09:00:00';
+    private const VOTING_END_AT   = '2020-06-29 23:59:00';
+
+    public function __construct()
+    {
+    }
+
+    public function __invoke(VotingTimeQuery $_) : VotingTime
+    {
+        return VotingTime::fromFormat('Y-m-d H:i:s', self::VOTING_BEGIN_AT, self::VOTING_END_AT);
+    }
+}
