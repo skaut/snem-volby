57db9dba2826319b2eb0a89ba19e917e916a8634
Jan Staněk <jan@jstanek.cz>
2020-06-17 11:56:52 +0200

    filter pro datetime format

--- app/Utils/Helpers.php
+++ app/Utils/Helpers.php
@@ -15,4 +15,15 @@ class Helpers
      * @var string
      */
     public const DATETIME_FORMAT = 'j. n. Y H:i';
+
+
+    public static function formatDate(\DateTimeImmutable $datetime) : string
+    {
+        return $datetime->format(self::DATE_FORMAT);
+    }
+
+    public static function formatDateTime(\DateTimeImmutable $datetime) : string
+    {
+        return $datetime->format(self::DATETIME_FORMAT);
+    }
 }
