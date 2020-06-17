898a7765e3a2116abdd7cab86d5690da52d3caaf
Jan Staněk <jan@jstanek.cz>
2020-06-18 00:33:11 +0200

    přejmenování sloupce option

--- migrations/Version20200614153338.php
+++ migrations/Version20200614153338.php
@@ -12,10 +12,12 @@ final class Version20200614153338 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql('CREATE UNIQUE INDEX UNIQ_9599CB6A217BBB47 ON users_vote (person_id)');
+        $this->addSql('ALTER TABLE vote CHANGE COLUMN `option` choice VARCHAR(255)');
     }
 
     public function down(Schema $schema) : void
     {
         $this->addSql('DROP INDEX UNIQ_9599CB6A217BBB47 ON users_vote');
+        $this->addSql('ALTER TABLE vote CHANGE COLUMN choice `option` VARCHAR(255)');
     }
 }
