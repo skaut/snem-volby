7c92c4b4bc7eabfb89ce79c7f51ff33b32f03606
František Hána <sinacek@gmail.com>
2020-06-21 19:47:39 +0200

    fix component

--- app/Components/VotingStateBox.php
+++ app/Components/VotingStateBox.php
@@ -11,17 +11,20 @@ use Model\Config\ReadModel\Queries\VotingTimeQuery;
 use Model\Delegate\ReadModel\Queries\DelegatesCountQuery;
 use Model\UserService;
 use Model\Vote\ReadModel\Queries\VotingResultQuery;
+use Nette\Security\User;
 
 final class VotingStateBox extends BaseControl
 {
     /** @var QueryBus */
     private $queryBus;
     private UserService $userService;
+    private User $user;
 
-    public function __construct(QueryBus $queryBus, UserService $userService)
+    public function __construct(QueryBus $queryBus, UserService $userService, User $user)
     {
         $this->queryBus    = $queryBus;
         $this->userService = $userService;
+        $this->user        = $user;
     }
 
     public function render(bool $showUnpublished = false) : void
@@ -32,7 +35,7 @@ final class VotingStateBox extends BaseControl
             'votingTime'    => $this->queryBus->handle(new VotingTimeQuery()),
             'votingResult' => $this->queryBus->handle(new VotingResultQuery()),
             'delegatesCount'  => $this->queryBus->handle(new DelegatesCountQuery()),
-            'showResult' => $this->queryBus->handle(new VotingPublishedQuery()) !== null || ($this->userService->isSuperUser() && $showUnpublished),
+            'showResult' => $this->queryBus->handle(new VotingPublishedQuery()) !== null || ($this->user->isLoggedIn () && $this->userService->isSuperUser() && $showUnpublished),
         ]);
 
         $this->template->render();
