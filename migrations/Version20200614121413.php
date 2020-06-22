a3c07cfa224631077437319c8759f507b2e1940c
František Hána <sinacek@gmail.com>
2020-06-22 16:09:06 +0200

    format migrations

--- migrations/Version20200614121413.php
+++ migrations/Version20200614121413.php
@@ -11,8 +11,22 @@ final class Version20200614121413 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->addSql('CREATE TABLE users_vote (id INT AUTO_INCREMENT NOT NULL, person_id INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, `option` VARCHAR(255) NOT NULL COMMENT \'(DC2Type:string_enum)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql(<<<EOT
+CREATE TABLE users_vote (
+    id INT AUTO_INCREMENT NOT NULL,
+    person_id INT NOT NULL,
+    created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+    PRIMARY KEY(id)
+) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB
+EOT);
+        $this->addSql(<<<EOT
+CREATE TABLE vote (
+    id INT AUTO_INCREMENT NOT NULL,
+    `option` VARCHAR(255) NOT NULL COMMENT '(DC2Type:string_enum)',
+    created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+    PRIMARY KEY(id)
+) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB
+EOT);
     }
 
     public function down(Schema $schema) : void
